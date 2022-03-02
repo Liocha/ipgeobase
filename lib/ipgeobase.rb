55bbc3019f68babd48ffc81a24d197d124d1b77b
Alex <dev@liocha.ru>
2022-03-03 02:13:12 +0300

    corrected by review

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -5,10 +5,10 @@ autoload(:IpMeta, 'ipgeobase/ip_meta.rb')
 
 # top-level documentation comment for module Ipgeobase
 module Ipgeobase
-  class Error < StandardError; end
+  $endpoint_api = 'http://ip-api.com/xml/'
 
   def self.lookup(ip)
-    data = Net::HTTP.get(URI("http://ip-api.com/xml/#{ip}"))
+    data = Net::HTTP.get(URI("#{$endpoint_api}#{ip}"))
     IpMeta.parse(data)
   end
 end
